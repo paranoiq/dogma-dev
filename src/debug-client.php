f9e9d75d4f5d70e862ce6f405c19eaff79503f5b
Vlasta Neubauer <cz.paranoiq@gmail.com>
2018-07-12 13:04:13 +0200

    Debug tools may be loaded twice

--- src/debug-client.php
+++ src/debug-client.php
@@ -16,92 +16,91 @@ require_once __DIR__ . '/../../../tracy/tracy/src/tracy.php';
 use Tracy\Debugger;
 use Tracy\Dumper;
 
-/**
- * @param mixed ...$params
- */
-function d(...$params): void
-{
-    Debugger::dump(...$params);
-}
-
-/**
- * @param mixed ...$params
- */
-function bd(...$params): void
-{
-    Debugger::barDump(...$params);
-}
-
-/**
- * @param mixed $value
- * @param int $depth
- */
-function rdx($value, int $depth = 5): void
-{
-    rd($value, $depth, false);
-}
-
-/**
- * @param mixed $value
- * @param mixed|null $name
- * @param int|bool $depth
- * @param bool $showTrace
- */
-function rd($value, $name = null, $depth = 5, $showTrace = true): void
-{
-    static $n;
-
-    if ($depth === false) {
-        $depth = 5;
-        $showTrace = false;
+if (!function_exists('d')) {
+    /**
+     * @param mixed ...$params
+     */
+    function d(...$params): void
+    {
+        Debugger::dump(...$params);
     }
+}
 
-    if ($n === null) {
-        $message = "\n" . date('Y-m-d H:i:s') . " ----------------------------------------------------------------------------------------------------------------\n";
+if (!function_exists('bd')) {
+    /**
+     * @param mixed ...$params
+     */
+    function bd(...$params): void
+    {
+        Debugger::barDump(...$params);
+    }
+}
+
+if (!function_exists('rd')) {
+    /**
+     * @param mixed $value
+     * @param mixed|null $name
+     * @param int|bool $depth
+     * @param bool $showTrace
+     */
+    function rd($value, $name = null, $depth = 5, $showTrace = true): void
+    {
+        static $n;
+
+        if ($depth === false) {
+            $depth = 5;
+            $showTrace = false;
+        }
+
+        if ($n === null) {
+            $message = "\n" . date('Y-m-d H:i:s') . " ----------------------------------------------------------------------------------------------------------------\n";
+            remoteDebugWrite($message);
+            $n = 0;
+        }
+
+        if ($value instanceof \Dogma\Time\DateOrTime || $value instanceof \Dogma\Math\Interval\Interval) {
+            // fill internal cache
+            $value->format();
+        }
+
+        $options = [
+            Dumper::DEPTH => $depth,
+            Dumper::TRUNCATE => 1000,
+            Dumper::LOCATION => false,
+        ];
+        $dump = Dumper::toTerminal($value, $options);
+        $message = ($name ? $name . ': ' : '') . trim($dump) . "\n";
+        if ($showTrace) {
+            $trace = debug_backtrace();
+            $message .= 'in ' . ($trace[0]['file'] ?? '?') . ':' . ($trace[0]['line'] ?? '?') . " ($n)\n";
+        }
+
         remoteDebugWrite($message);
-        $n = 0;
+        $n++;
     }
-
-    if ($value instanceof \Dogma\Time\DateOrTime || $value instanceof \Dogma\Math\Interval\Interval) {
-        // fill internal cache
-        $value->format();
-    }
-
-    $options = [
-        Dumper::DEPTH => $depth,
-        Dumper::TRUNCATE => 1000,
-        Dumper::LOCATION => false,
-    ];
-    $dump = Dumper::toTerminal($value, $options);
-    $message = ($name ? $name . ': ' : '') . trim($dump) . "\n";
-    if ($showTrace) {
-        $trace = debug_backtrace();
-        $message .= 'in ' . ($trace[0]['file'] ?? '?') . ':' . ($trace[0]['line'] ?? '?') . " ($n)\n";
-    }
-
-    remoteDebugWrite($message);
-    $n++;
 }
 
-function remoteDebugWrite(string $message): void
-{
-    static $socket;
+if (!function_exists('debugWrite')) {
+    function remoteDebugWrite(string $message): void
+    {
+        static $socket;
 
-    if ($socket === null) {
-        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-        if (!$socket) {
-            die("Could not create socket to debug server.\n");
+        if ($socket === null) {
+            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+            if (!$socket) {
+                die("Could not create socket to debug server.\n");
+            }
+
+            $result = socket_connect($socket, '127.0.0.1', 6666);
+            if (!$result) {
+                die("Could not connect to debug server.\n");
+            }
         }
 
-        $result = socket_connect($socket, '127.0.0.1', 6666);
+        $result = socket_write($socket, $message, strlen($message));
         if (!$result) {
-            die("Could not connect to debug server.\n");
+            die("Could not send data to debug server.\n");
         }
-    }
-
-    $result = socket_write($socket, $message, strlen($message));
-    if (!$result) {
-        die("Could not send data to debug server.\n");
     }
 }
 
